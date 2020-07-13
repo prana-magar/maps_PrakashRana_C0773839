e5e46a8303d9de99d1b511bb92dd509a5da89593
prana <prakashrana9847067614@gmail.com>
2020-07-12 23:54:04 -0400

    inital commit

---
+++ app/src/main/java/com/example/maps_prakashrana_c0773839/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.maps_prakashrana_c0773839;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
